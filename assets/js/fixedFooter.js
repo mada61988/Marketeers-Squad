17b69db44fd463c2333512be8f4dd6bead3f114e
mada61988 <69260885+mada61988@users.noreply.github.com>
2021-05-23 02:07:20 -0700

    t

--- assets/js/fixedFooter.js
+++ assets/js/fixedFooter.js
@@ -1,7 +1,7 @@
 function fixedFooter(){
     console.log('back to top function called')
     console.log(window.location)
-  if(window.innerWidth<700 && window.location.pathname=='/Marketeers-Squad/blog.html'){
+  if(window.innerWidth<700 ){
     document.getElementById('course-enrollment').style.display="none"
   document.getElementById('fixed-footer').style.display ="block"
   document.getElementById('back-to-top-blog-page').style.marginBottom='80px'

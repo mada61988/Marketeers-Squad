5e74d93e80511014aeff3e9ba28e9f6337c6902c
mada61988 <69260885+mada61988@users.noreply.github.com>
2021-07-01 17:40:52 +0200

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -701,6 +701,48 @@ $(".sidebar-box-why-we-rock .button").click(function() {
 //=================================================================================
 
 
+
+//===============================================================================
+// our vision readmore action 
+//=================================================================================
+
+$(".sidebar-box-our-vision .button").click(function() {
+      
+  totalHeight = 0;
+
+  $el = $(this);
+  $p  = $el.parent();
+  $up = $p.parent();
+  $ps = $up.find("p:not('.read-more-our-vision')");
+  
+  // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
+  $ps.each(function() {
+    totalHeight += $(this).outerHeight();
+  });
+        
+  $up
+    .css({
+      // Set height to prevent instant jumpdown when max height is removed
+      "height": $up.height(),
+      "max-height": 9999
+    })
+    .animate({
+      "height": totalHeight
+    });
+  
+  // fade out read-more
+  $p.fadeOut();
+  
+  // prevent jump-down
+  return false;
+    
+}); 
+
+//===============================================================================
+// our vision readmore action 
+//=================================================================================
+
+
 //===============================================================================
 // content-creation readmore action 
 //=================================================================================
